18d2c063cdb3da51e08b60c71836a8b97c729010
leftan <leftan12121@gmail.com>
2016-08-16 01:13:48 -0700

    mimsmind_final

--- mimsmind1.py
+++ mimsmind1.py
@@ -7,10 +7,8 @@ def str_to_tuple(s):
         lst += [(s[n], len(s) - 1 - n)]
     return lst
 
-# print(str_to_tuple('975'))
-# print(str_to_tuple('95'))
 
-def check_bull_cow(l1, l2): # this function has error: see example below
+def check_bull_cow(l1, l2): # this function has error
     '''d1, d2: dict'''
     count_bull = 0 
     count_cow = 0
@@ -29,22 +27,14 @@ def num_format(num, len_digit=3):
     result = "0" * (len_digit - len(str(num))) + str(num)
     return result
 
-l1 = str_to_tuple(num_format(338))
-l2 = str_to_tuple(num_format(3))
 
-print(check_bull_cow(l1, l2))
-
-# print(num_format('78'))
-# print(type(num_format('78')))
 
 def mimsmind1(len_digit=3):
     count = 0
     maxrounds = 2 ** len_digit + len_digit
     guess = None
     num = random.randint(0, 10 ** len_digit - 1)
-    print(num)
     lst_random = str_to_tuple(num_format(num))
-    print(lst_random)
     print("Let's play the mimsmind1 game. You have {} guesses.".format(maxrounds))
     prompt = "Guess a {}-digit number: ".format(len_digit)
     prompt_other = "Try again: "
@@ -75,7 +65,7 @@ def mimsmind1(len_digit=3):
         
 
 def main():
-    mimsmind1(3)
+    mimsmind1()
 
 if __name__ == "__main__":
     main()
